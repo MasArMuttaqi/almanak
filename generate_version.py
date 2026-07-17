06cf404216567c358587353d52f420962fa40afa
MasArMuttaqi <140738717+MasArMuttaqi@users.noreply.github.com>
2026-07-17 03:42:16 +0000

    version update

--- generate_version.py
+++ generate_version.py
@@ -28,7 +28,7 @@ def write_version_file():
     
     commit_count_1 = int(commit_count) + 4 
     # Format versi dasar aplikasi Anda
-    major_minor = "2.8"
+    major_minor = "2.9"
     
     version_data = {
         "version": f"v{major_minor}.{commit_count_1}",
